89c6e0cdfb9773cdba4528d3f789260dda51c7e0
tedLeary <markeleary@gmail.com>
2018-09-18 11:50:29 -0700

    align nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,12 +5,12 @@ export default function  Nav(){
   return(
     <div className={styles.navWrapper}>
       <div className={styles.navLinks}>
-        <ul className={styles.navLinkList}>
-          <li><a>Home</a></li>
-          <li><a>About Me</a></li>
-          <li><a>Work</a></li>
-          <li><a>Contact</a></li>
-        </ul>
+        <div className={styles.navLinkList}>
+          <a>Home</a>
+          <a>About Me</a>
+          <a>Work</a>
+          <a>Contact</a>
+        </div>
       </div>
       <div className={styles.navSocial}></div>
 
