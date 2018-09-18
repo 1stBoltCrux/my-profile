0d7524ad64868a5d03758c1601695aad23120643
tedLeary <markeleary@gmail.com>
2018-09-18 12:53:36 -0700

    refactor navbar for dynamic rendering

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styles from './nav.scss';
+
+import { Link } from 'react-router'
+
 console.log(styles);
-export default function  Nav(){
+
+export default class Nav extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      windowSize: window.innerWidth,
+      navDropdownVisible: false
+    }
+  }
+
+  componentDidMount(){
+    window.addEventListener('resize', this.handleSize)
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('resize', this.handleSize)
+  }
+
+
+  handleSize = () => {
+    console.log(window.innerWidth);
+    this.setState({
+      windowSize: window.innerWidth
+    })
+  }
+  render(){
+
+if (this.state.windowSize > 560) {
   return(
     <div className={styles.navWrapper}>
       <div className={styles.navLinks}>
@@ -11,9 +41,17 @@ export default function  Nav(){
           <a>Work</a>
           <a>Contact</a>
         </div>
+
       </div>
       <div className={styles.navSocial}></div>
 
     </div>
   )
+
+} else {
+  return (
+    <p>hey there guy</p>
+  )
+}
+}
 }
