25b7e14fcc7959b4d5eef89bc96b108ef6c4c2d6
tedLeary <markeleary@gmail.com>
2018-09-19 11:54:41 -0700

    finalize link animation styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,10 @@ import linkedin from './assets/linkedin.png'
 export default function Footer(){
   return(
     <div className={styles.footerWrapper}>
+      <h3>Contact</h3>
       <div className={styles.iconContainer}>
         <img src={envelope} alt='email envelope'/>
-        <img src={linkedin} alt='linkedin'/>  
+        <img src={linkedin} alt='linkedin'/>
       </div>
     </div>
   )
